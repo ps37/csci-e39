89ba27069abaca3aaf0c52430292d7d4a752127a
psimhadri <prudhvi.af121@gmail.com>
2018-04-22 10:48:41 -0400

    Date formatting

--- src/assignments/project-2/components/message/message.component.jsx.js
+++ src/assignments/project-2/components/message/message.component.jsx.js
@@ -8,11 +8,33 @@ class MessageComponent extends React.Component {
         autobind(this);
     }
 
+    formatDate(date) {
+      const today = new Date();
+      const timeDiff = Math.abs(today.getTime() - date.getTime());
+      const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+      if (diffDays > 1) {
+        return date.toLocaleString("en-US", {
+            hour12: true,
+            localeMatcher: "lookup",
+        })
+      };
+      const localeSpecificTime = date.toLocaleTimeString("en-US", {
+          hour12: true,
+          localeMatcher: "lookup"
+      });
+      return localeSpecificTime.replace(/:\d+ /, ' ');
+    };
+
     render() {
         const {message}=this.props;
 
         return  <div key={message.id}>
-                    <label>{message.student.name} at {message.createdAt.toISOString()}</label>
+                    <label>
+                        {message.student.name}
+                        <span className="message-date-string">
+                            {this.formatDate(message.createdAt)}
+                        </span>
+                    </label>
                     <p>{message.text}</p>
                 </div>
     }
